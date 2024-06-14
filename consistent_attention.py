f6a5f2d3033ea9b451d2faaafff471d881a2814e
Theodore <tinprocoder0908@gmail.com>
2024-06-15 00:12:47 +0700

    update code

--- consistent_attention.py
+++ consistent_attention.py
@@ -10,10 +10,23 @@ else:
 
 
 class SpatialAttnProcessorV2(torch.nn.Module):
+    r"""
+    Attention processor for IP-Adapater for PyTorch 2.0.
+    Args:
+        hidden_size (`int`):
+            The hidden size of the attention layer.
+        cross_attention_dim (`int`):
+            The number of channels in the `encoder_hidden_states`.
+        text_context_len (`int`, defaults to 77):
+            The context length of the text features.
+        scale (`float`, defaults to 1.0):
+            the weight scale of image prompt.
+    """
+
     def __init__(self, hidden_size=None, cross_attention_dim=None, id_length=4, device="cuda", dtype=torch.float16):
         super().__init__()
         if not hasattr(F, "scaled_dot_product_attention"):
-            raise ImportError("AttnProcessor2_0 requires PyTorch 2.0. Please upgrade PyTorch to 2.0.")
+            raise ImportError("AttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
         self.device = device
         self.dtype = dtype
         self.hidden_size = hidden_size
@@ -21,16 +34,6 @@ class SpatialAttnProcessorV2(torch.nn.Module):
         self.total_length = id_length + 1
         self.id_length = id_length
         self.id_bank = {}
-        self.total_count = 0
-        self.attn_count = 0
-        self.cur_step = 0
-        self.mask1024 = None
-        self.mask4096 = None
-        self.sa32 = None
-        self.sa64 = None
-        self.write = False
-        self.height = 0
-        self.width = 0
 
     def __call__(
             self,
@@ -39,39 +42,47 @@ class SpatialAttnProcessorV2(torch.nn.Module):
             encoder_hidden_states=None,
             attention_mask=None,
             temb=None):
-        if self.write:
-            self.id_bank[self.cur_step] = [hidden_states[:self.id_length], hidden_states[self.id_length:]]
+        global total_count, attn_count, cur_step, mask1024, mask4096
+        global sa32, sa64
+        global write
+        global height, width
+        if write:
+            # print(f"white:{cur_step}")
+            self.id_bank[cur_step] = [hidden_states[:self.id_length], hidden_states[self.id_length:]]
         else:
-            encoder_hidden_states = torch.cat((self.id_bank[self.cur_step][0].to(self.device), hidden_states[:1],
-                                               self.id_bank[self.cur_step][1].to(self.device), hidden_states[1:]))
-        # skip in early steps
-        if self.cur_step < 5:
+            encoder_hidden_states = torch.cat((self.id_bank[cur_step][0].to(self.device), hidden_states[:1],
+                                               self.id_bank[cur_step][1].to(self.device), hidden_states[1:]))
+        # skip in early step
+        if cur_step < 5:
             hidden_states = self.__call2__(attn, hidden_states, encoder_hidden_states, attention_mask, temb)
         else:  # 256 1024 4096
             random_number = random.random()
-            rand_num = 0.3 if self.cur_step < 20 else 0.1
+            if cur_step < 20:
+                rand_num = 0.3
+            else:
+                rand_num = 0.1
             if random_number > rand_num:
-                if not self.write:
-                    if hidden_states.shape[1] == (self.height // 32) * (self.width // 32):
-                        attention_mask = self.mask1024[self.mask1024.shape[0] // self.total_length * self.id_length:]
+                if not write:
+                    if hidden_states.shape[1] == (height // 32) * (width // 32):
+                        attention_mask = mask1024[mask1024.shape[0] // self.total_length * self.id_length:]
                     else:
-                        attention_mask = self.mask4096[self.mask4096.shape[0] // self.total_length * self.id_length:]
+                        attention_mask = mask4096[mask4096.shape[0] // self.total_length * self.id_length:]
                 else:
-                    if hidden_states.shape[1] == (self.height // 32) * (self.width // 32):
-                        attention_mask = self.mask1024[:self.mask1024.shape[0] // self.total_length * self.id_length,
-                                         :self.mask1024.shape[0] // self.total_length * self.id_length]
+                    if hidden_states.shape[1] == (height // 32) * (width // 32):
+                        attention_mask = mask1024[:mask1024.shape[0] // self.total_length * self.id_length,
+                                         :mask1024.shape[0] // self.total_length * self.id_length]
                     else:
-                        attention_mask = self.mask4096[:self.mask4096.shape[0] // self.total_length * self.id_length,
-                                         :self.mask4096.shape[0] // self.total_length * self.id_length]
+                        attention_mask = mask4096[:mask4096.shape[0] // self.total_length * self.id_length,
+                                         :mask4096.shape[0] // self.total_length * self.id_length]
                 hidden_states = self.__call1__(attn, hidden_states, encoder_hidden_states, attention_mask, temb)
             else:
                 hidden_states = self.__call2__(attn, hidden_states, None, attention_mask, temb)
-        self.attn_count += 1
-        if self.attn_count == self.total_count:
-            self.attn_count = 0
-            self.cur_step += 1
-            self.mask1024, self.mask4096 = cal_attn_mask_xl(self.total_length, self.id_length, self.sa32, self.sa64,
-                                                            self.height, self.width, device=self.device, dtype=self.dtype)
+        attn_count += 1
+        if attn_count == total_count:
+            attn_count = 0
+            cur_step += 1
+            mask1024, mask4096 = cal_attn_mask_xl(self.total_length, self.id_length, sa32, sa64, height, width,
+                                                  device=self.device, dtype=self.dtype)
 
         return hidden_states
 
@@ -81,7 +92,8 @@ class SpatialAttnProcessorV2(torch.nn.Module):
             hidden_states,
             encoder_hidden_states=None,
             attention_mask=None,
-            temb=None):
+            temb=None,
+    ):
         residual = hidden_states
         if attn.spatial_norm is not None:
             hidden_states = attn.spatial_norm(hidden_states, temb)
@@ -107,7 +119,7 @@ class SpatialAttnProcessorV2(torch.nn.Module):
         else:
             encoder_hidden_states = encoder_hidden_states.view(-1, self.id_length + 1, nums_token, channel).reshape(-1,
                                                                                                                     (
-                                                                                                                            self.id_length + 1) * nums_token,
+                                                                                                                                self.id_length + 1) * nums_token,
                                                                                                                     channel)
 
         key = attn.to_k(encoder_hidden_states)
@@ -158,9 +170,14 @@ class SpatialAttnProcessorV2(torch.nn.Module):
             batch_size, channel, height, width = hidden_states.shape
             hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
 
-        batch_size, sequence_length, channel = hidden_states.shape
+        batch_size, sequence_length, channel = (
+            hidden_states.shape
+        )
+        # print(hidden_states.shape)
         if attention_mask is not None:
             attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
+            # scaled_dot_product_attention expects attention_mask shape to be
+            # (batch, heads, source_length, target_length)
             attention_mask = attention_mask.view(batch_size, attn.heads, -1, attention_mask.shape[-1])
 
         if attn.group_norm is not None:
@@ -171,8 +188,10 @@ class SpatialAttnProcessorV2(torch.nn.Module):
         if encoder_hidden_states is None:
             encoder_hidden_states = hidden_states  # B, N, C
         else:
-            encoder_hidden_states = encoder_hidden_states.view(-1, self.id_length + 1, sequence_length, channel).reshape(
-                -1, (self.id_length + 1) * sequence_length, channel)
+            encoder_hidden_states = encoder_hidden_states.view(-1, self.id_length + 1, sequence_length,
+                                                               channel).reshape(-1,
+                                                                                (self.id_length + 1) * sequence_length,
+                                                                                channel)
 
         key = attn.to_k(encoder_hidden_states)
         value = attn.to_v(encoder_hidden_states)
